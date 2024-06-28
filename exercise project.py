85a0640203f968bf1e5f8c1e96285dbac66451a9
ParsaOfRivia <120216788+ParsaOfRivia@users.noreply.github.com>
2024-06-28 07:38:30 +0330

    Add files via upload

---
+++ exercise project.py
@@ -0,0 +1,401 @@
+#Mohammadparsa Salehi and Mahtab Seyedmirzaeis' Project
+
+import cv2
+import mediapipe as mp
+import numpy as np
+import time
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+mp_pose = mp.solutions.pose
+
+
+def calculate_angle(a, b, c):
+    a = np.array(a)  # First
+    b = np.array(b)  # Mid
+    c = np.array(c)  # End
+
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+
+    if angle > 180.0:
+        angle = 360 - angle
+
+    return angle
+
+counter = 0
+stage = None
+
+
+# For webcam input:
+cap = cv2.VideoCapture(0)
+print("Choose one with your hand : \n dumbell number 5 \n for Side Lateral Raise number 1 \n Triceps number 2")
+with mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+    start_time = None
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+          print("Ignoring empty camera frame.")
+          # If loading a video, use 'break' instead of 'continue'.
+          continue
+
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        frame.flags.writeable = False
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results_hand = hands.process(image)
+        # Draw the hand annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Initially set finger count to 0 for each cap
+        fingerCount = 0
+
+        if results_hand.multi_hand_landmarks:
+
+          for hand_landmarks in results_hand.multi_hand_landmarks:
+            # Get hand index to check label (left or right)
+            handIndex = results_hand.multi_hand_landmarks.index(hand_landmarks)
+            handLabel = results_hand.multi_handedness[handIndex].classification[0].label
+
+            # Set variable to keep landmarks positions (x and y)
+            handLandmarks = []
+
+            # Fill list with x and y positions of each landmark
+            for landmarks_hand in hand_landmarks.landmark:
+              handLandmarks.append([landmarks_hand.x, landmarks_hand.y])
+
+            # Test conditions for each finger: Count is increased if finger is
+            #   considered raised.
+            # Thumb: TIP x position must be greater or lower than IP x position,
+            #   deppeding on hand label.
+            if handLabel == "Left" and handLandmarks[4][0] > handLandmarks[3][0]:
+              fingerCount = fingerCount+1
+            elif handLabel == "Right" and handLandmarks[4][0] < handLandmarks[3][0]:
+              fingerCount = fingerCount+1
+
+            # Other fingers: TIP y position must be lower than PIP y position,
+            #   as image origin is in the upper left corner.
+            if handLandmarks[8][1] < handLandmarks[6][1]:       #Index finger
+              fingerCount = fingerCount+1
+            if handLandmarks[12][1] < handLandmarks[10][1]:     #Middle finger
+              fingerCount = fingerCount+1
+            if handLandmarks[16][1] < handLandmarks[14][1]:     #Ring finger
+              fingerCount = fingerCount+1
+            if handLandmarks[20][1] < handLandmarks[18][1]:     #Pinky
+              fingerCount = fingerCount+1
+
+            # Draw hand landmarks
+            # mp_drawing.draw_landmarks(
+            #     image,
+            #     hand_landmarks,
+            #     mp_hands.HAND_CONNECTIONS,
+            #     mp_drawing_styles.get_default_hand_landmarks_style(),
+            #     mp_drawing_styles.get_default_hand_connections_style())
+            # print(fingerCount)
+        # Display finger count
+        cv2.putText(image, str(fingerCount), (50, 450), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 10)
+        cv2.putText(image, "Choose based on the terminal shown" ,(300,50), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0,255,0), 1)
+        if fingerCount == 5 and start_time is None:
+            start_time = time.time()
+        if fingerCount in [4,3,6,7,8,9,10]:
+            start_time = None
+        #######
+        if fingerCount == 1 and start_time is None:
+            start_time = time.time()
+        if fingerCount in [4,3,6,7,8,9,10]:
+            start_time = None
+        #######
+        if fingerCount == 2 and start_time is None:
+            start_time = time.time()
+        if fingerCount in [4,3,6,7,8,9,10]:
+            start_time = None
+        # Display image
+        if start_time is not None and time.time() - start_time >= 3:
+            break
+        cv2.imshow('MediaPipe Feed', image)
+        if cv2.waitKey(5) & 0xFF == 27:
+          break
+cap.release()
+cap = cv2.VideoCapture(0)
+# for Dumbbell
+if fingerCount==5:
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret, frame = cap.read()
+
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+
+            # Make detection
+            results = pose.process(image)
+
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            # Extract landmarks
+            try:
+                landmarks = results.pose_landmarks.landmark
+
+                # Get coordinates
+                shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                            landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                         landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                         landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+                # Calculate angle
+                angle = calculate_angle(shoulder, elbow, wrist)
+
+                # Visualize angle
+                cv2.putText(image, str(angle),
+                            tuple(np.multiply(elbow, [640, 480]).astype(int)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                            )
+
+                # Curl counter logic
+                if angle > 160:
+                    stage = "down"
+                if angle < 30 and stage == 'down':
+                    stage = "up"
+                    counter += 1
+                    print(counter)
+
+            except:
+                pass
+
+            # Render curl counter
+            # Setup status box
+            cv2.rectangle(image, (0, 0), (225, 73), (245, 117, 16), -1)
+
+            # Rep data
+            cv2.putText(image, 'REPS', (15, 12),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(counter),
+                        (10, 60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Stage data
+            cv2.putText(image, 'STAGE', (65, 12),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, stage,
+                        (60, 60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Render detections
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                      mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                      )
+
+            cv2.imshow('Mediapipe Feed', image)
+
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+# for Side Lateral Raise
+if fingerCount==1:
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret, frame = cap.read()
+
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+
+            # Make detection
+            results = pose.process(image)
+
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            # Extract landmarks
+            try:
+                landmarks = results.pose_landmarks.landmark
+
+                # Get coordinates
+                right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                            landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
+                         landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+                right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
+                         landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+
+                left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                            landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                         landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                left_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
+                         landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+
+                # Calculate angle
+                angle1 = calculate_angle(right_elbow, right_shoulder, right_hip)
+                angle2 = calculate_angle(left_elbow, left_shoulder, left_hip)
+
+                # Visualize angle
+                cv2.putText(image, str(angle2),
+                            tuple(np.multiply(left_shoulder, [640, 480]).astype(int)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                            )
+                cv2.putText(image, str(angle1),
+                            tuple(np.multiply(right_shoulder, [640, 480]).astype(int)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                            )
+
+                # Curl counter logic
+                if angle1 <30 and angle2<30:
+                    stage = "down"
+                if angle1 >70 and stage == 'down' and angle2>70:
+                    stage = "up"
+                    counter += 1
+                    print(counter)
+
+
+            except:
+                pass
+
+            # Render curl counter
+            # Setup status box
+            cv2.rectangle(image, (0, 0), (225, 73), (245, 117, 16), -1)
+
+            # Rep data
+            cv2.putText(image, 'REPS', (15, 12),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(counter),
+                        (10, 60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Stage data
+            cv2.putText(image, 'STAGE', (65, 12),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, stage,
+                        (60, 60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Render detections
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                      mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                      )
+
+            cv2.imshow('Mediapipe Feed', image)
+
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+# for Triceps
+if fingerCount == 2:
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret, frame = cap.read()
+
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+
+            # Make detection
+            results = pose.process(image)
+
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            # Extract landmarks
+            try:
+                landmarks = results.pose_landmarks.landmark
+
+                # Get coordinates
+                right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
+                                  landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+                right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                               landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
+                             landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+                left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                               landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                                  landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                left_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
+                             landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+                right_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                                 landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+                left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                            landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+
+                # Calculate angle
+                angle1 = calculate_angle(right_elbow, right_shoulder, right_hip)
+                angle2 = calculate_angle(left_elbow, left_shoulder, left_hip)
+                angle3 = calculate_angle(right_wrist, right_elbow, right_shoulder)
+                angle4 = calculate_angle(left_wrist, left_elbow, left_shoulder)
+
+
+                # Visualize angle
+                cv2.putText(image, str(angle3),
+                            tuple(np.multiply(right_elbow, [640, 480]).astype(int)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                            )
+                cv2.putText(image, str(angle4),
+                            tuple(np.multiply(left_elbow, [640, 480]).astype(int)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                            )
+
+                # Curl counter logic
+                if angle1>90 and angle2>90:
+                    if angle3 < 90 and angle4 < 90:
+                        stage = "down"
+                    if angle3 > 130 and stage == 'down' and angle4 > 130:
+                        stage = "up"
+                        counter += 1
+                        print(counter)
+
+
+            except:
+                pass
+
+            # Render curl counter
+            # Setup status box
+            cv2.rectangle(image, (0, 0), (225, 73), (245, 117, 16), -1)
+
+            # Rep data
+            cv2.putText(image, 'REPS', (15, 12),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(counter),
+                        (10, 60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Stage data
+            cv2.putText(image, 'STAGE', (65, 12),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, stage,
+                        (60, 60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Render detections
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                      mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                      )
+
+            cv2.imshow('Mediapipe Feed', image)
+
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+
